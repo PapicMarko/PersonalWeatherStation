6a0ae8d8e5ff5e2c437f4a067ffc2eaae69a06bb
Marko Papic <32815437+PapicMarko@users.noreply.github.com>
2024-12-03 15:44:05 +0100

    sensordata

--- sensor_code/sensor_code.ino
+++ sensor_code/sensor_code.ino
@@ -5,7 +5,7 @@
 
 // Definicija pinova za DHT22
 #define DHTPIN 2     // Digitalni pin na kojem je povezan DHT22
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define DHTTYPE DHT22   // DHT 22 (AM2302)
 DHT dht(DHTPIN, DHTTYPE);
 
 // Kreiranje instance za BMP280
@@ -17,11 +17,9 @@ int sensorValue = 0;
 float sensorVoltage = 0;
 float windSpeed = 0;
 float voltageConversionConstant = 0.004882814;
-int sensorDelay = 1000; // 1 sekunda delay za anemometar
 float voltageMin = 0.4;
-float windSpeedMin = 0;
 float voltageMax = 2.0;
-float windSpeedMax = 32;
+float windSpeedMax = 32.0; // Maksimalna brzina vjetra koju može mjeriti anemometar
 
 void setup() {
   Serial.begin(9600);
@@ -31,7 +29,7 @@ void setup() {
   dht.begin();
 
   // Start BMP280 senzor
-  if (!bmp.begin(0x77)) {   // Promijeni na 0x77 ako je adresa BMP280 senzora 0x77
+  if (!bmp.begin(0x77)) {   // Promijeni na 0x77 ako je adresa BMP280 senzora drugačija
     Serial.println(F("Nije moguće pronaći BMP280 senzor!"));
     while (1);
   }
@@ -39,24 +37,24 @@ void setup() {
 
 void loop() {
   // Čitanje vlažnosti i temperature s DHT22
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  float tempDHT = dht.readTemperature();
 
-  // Čitanje temperature i tlaka s BMP280
+  // Čitanje tlaka s BMP280
   float pressure = bmp.readPressure() / 100.0; // Konverzija u hPa
 
-  // Provjera jesu li čitanja uspješna za DHT22
-  if (isnan(h) || isnan(t)) {
+  // Provjera uspješnosti očitanja s DHT22
+  if (isnan(humidity) || isnan(tempDHT)) {
     Serial.println(F("Greška pri čitanju s DHT senzora!"));
   } else {
     Serial.print(F("Vlažnost: "));
-    Serial.print(h);
+    Serial.print(humidity);
     Serial.print(F("%  Temperatura: "));
-    Serial.print(t);
+    Serial.print(tempDHT);
     Serial.println(F("°C"));
   }
 
-  // Ispis tlaka
+  // Ispis tlaka s BMP280
   Serial.print(F("Tlak: "));
   Serial.print(pressure);
   Serial.println(F(" hPa"));
@@ -71,11 +69,12 @@ void loop() {
     windSpeed = (sensorVoltage - voltageMin) * windSpeedMax / (voltageMax - voltageMin);
   }
 
+  // Ispis brzine vjetra
   Serial.print("Napon: ");
   Serial.print(sensorVoltage);
   Serial.print(" V\tBrzina vjetra: ");
   Serial.print(windSpeed);
   Serial.println(" m/s");
 
-  delay(300000); // Čekanje tri sekunde prije sljedećeg čitanja
+  delay(300000); // Čekanje 5 minuta prije sljedećeg čitanja
 }
